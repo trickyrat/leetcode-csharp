8c7f7c15ea8b63e09d29202870757891306e5056
trickyrat <wangjian0513@outlook.com>
2018-09-17 22:24:13 +0800

    add find the minimum value in rotated array

--- Solution.cs
+++ Solution.cs
@@ -1675,6 +1675,33 @@ namespace Solutions
         }
 
         /// <summary>
+        /// 153. Find Minimum in Rotated Sorted Array
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int FindMin(int[] nums)
+        {
+            if (nums.Length == 1) return nums[0];
+            int l = 0, r = nums.Length - 1;
+            if (nums[r] > nums[0])
+                return nums[0];
+            while (r >= l)
+            {
+                int mid = l + (r - l) / 2;
+                if (nums[mid] > nums[mid + 1])
+                    return nums[mid + 1];
+                if (nums[mid - 1] > nums[mid])
+                    return nums[mid];
+
+                if (nums[mid] > nums[0])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
+            return -1;
+        }
+
+        /// <summary>
         /// 162. Find Peak Element
         /// </summary>
         /// <param name="nums"></param>
