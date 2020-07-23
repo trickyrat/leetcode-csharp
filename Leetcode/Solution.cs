e0b54c1e647daf218a92a055a9851ed5e9b5948c
王剑 <wangjian0513@outlook.com>
2020-07-23 15:17:35 +0800

    add 64. Minimum Path Sum

--- Leetcode/Solution.cs
+++ Leetcode/Solution.cs
@@ -1839,6 +1839,43 @@ namespace Leetcode
         }
 
         /// <summary>
+        /// 64. Minimum Path Sum
+        /// </summary>
+        /// <param name="grid"></param>
+        /// <returns></returns>
+        public static int MinPathSum(int[][] grid)
+        {
+            int width = grid[0].Length;
+            int height = grid.Length;
+            int[][] dp = new int[height][];
+            for (int i = 0; i < height; i++)
+            {
+                dp[i] = new int[width];
+            }
+            dp[0][0] = grid[0][0];
+            // 第一行
+            for (int i = 1; i < width; i++)
+            {
+                dp[0][i] = dp[0][i - 1] + grid[0][i];
+            }
+
+            // 第一列
+            for (int i = 1; i < height; i++)
+            {
+                dp[i][0] = dp[i - 1][0] + grid[i][0];
+            }
+
+            for (int i = 1; i < height; i++)
+            {
+                for (int j = 1; j < width; j++)
+                {
+                    dp[i][j] = Math.Min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                }
+            }
+            return dp[height - 1][width - 1];
+        }
+
+        /// <summary>
         /// 66. Plus One 
         /// </summary>
         /// <param name="digits"></param>
