5e919e0e5290353de58dd44a86fd73f155d80730
wangjian <wangjian0513@outlook.com>
2021-03-02 01:27:10 +0800

    add 304 Range Sum Query 2D-Immutable

---
+++ Leetcodecsharp.Test/NumMatrixUnitTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Leetcodecsharp.Test
+{
+    public class NumMatrixUnitTest
+    {
+        private int[][] matrix =
+                        {
+                new int[5]{ 3,0,1,4,2},
+                new int[5]{ 5,6,3,2,1},
+                new int[5]{ 1,2,0,1,5},
+                new int[5]{ 4,1,0,1,7},
+                new int[5]{ 1,0,3,0,5},
+            };
+
+        [Fact]
+        public void TestMethod1()
+        {
+            NumMatrix nm = new NumMatrix(matrix);
+            int actual = nm.SumRange(2, 1, 4, 3);
+            int expected = 8;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMethod2()
+        {
+            NumMatrix nm = new NumMatrix(matrix);
+            int actual = nm.SumRange(1, 1, 2, 2);
+            int expected = 11;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMethod3()
+        {
+            NumMatrix nm = new NumMatrix(matrix);
+            int actual = nm.SumRange(1, 2, 2, 4);
+            int expected = 12;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
--- Leetcodecsharp/Program.cs
+++ Leetcodecsharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace Leetcodecsharp
 {
     class Program
