205bcd328dcc3f2771a5763d27366873d109594e
trickyrat <wangjian0513@outlook.com>
2021-04-28 10:33:18 +0800

    155. Min Stack

---
+++ Leetcodecsharp/MinStack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcodecsharp
+{
+    public class MinStack
+    {
+        public Stack<int> Min { get; protected set; }
+        public Stack<int> X { get; protected set; }
+
+        public MinStack()
+        {
+            Min = new Stack<int>();
+            X = new Stack<int>();
+            Min.Push(int.MaxValue);
+        }
+
+        public void Push(int val)
+        {
+            X.Push(val);
+            Min.Push(Math.Min(Min.Peek(), val));
+        }
+
+        public void Pop()
+        {
+            X.Pop();
+            Min.Pop();
+        }
+
+        public int Top()
+        {
+            return X.Peek();
+        }
+
+        public int GetMin()
+        {
+            return Min.Peek();
+        }
+    }
+}
