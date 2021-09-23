247bb13869d5d66b47e569bc35a7164f1003ecef
trickyrat <wangjian0513@outlook.com>
2021-09-23 16:29:43 +0800

    Add 567. Permutation in String

---
+++ Leetcodecsharp.Test/CheckInclusionUnitTest.cs
@@ -0,0 +1,26 @@
+﻿
+using Xunit;
+
+namespace Leetcodecsharp.Test
+{
+    public class CheckInclusionUnitTest
+    {
+        [Fact]
+        public void Test_Should_Return_True()
+        {
+            string s1 = "ab", s2 = "eidbaooo";
+            bool actual = Solution.CheckInclusion(s1, s2);
+            bool expected = true;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_Should_Return_False()
+        {
+            string s1 = "ab", s2 = "eidboaoo";
+            bool actual = Solution.CheckInclusion(s1, s2);
+            bool expected = false;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
--- Leetcodecsharp/Solution.cs
+++ Leetcodecsharp/Solution.cs
@@ -3414,6 +3414,70 @@ namespace Leetcodecsharp
         }
 
         /// <summary>
+        /// 567. Permutation in String
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns></returns>
+        public static bool CheckInclusion(string s1, string s2)
+        {
+            int n = s1.Length, m = s2.Length;
+            if (n > m)
+            {
+                return false;
+            }
+            int[] cnt = new int[26];
+            for (int i = 0; i < n; i++)
+            {
+                --cnt[s1[i] - 'a'];
+                ++cnt[s2[i] - 'a'];
+            }
+            int diff = 0;
+            foreach (int item in cnt)
+            {
+                if (item != 0)
+                {
+                    diff++;
+                }
+            }
+            if (diff == 0)
+            {
+                return true;
+            }
+            for (int i = n; i < m; i++)
+            {
+                int x = s2[i] - 'a', y = s2[i - n] - 'a';
+                if (x == y)
+                {
+                    continue;
+                }
+                if (cnt[x] == 0)
+                {
+                    ++diff;
+                }
+                ++cnt[x];
+                if (cnt[x] == 0)
+                {
+                    --diff;
+                }
+                if (cnt[y] == 0)
+                {
+                    ++diff;
+                }
+                --cnt[y];
+                if (cnt[y] == 0)
+                {
+                    --diff;
+                }
+                if (diff == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
         /// 617. Merge Two Binary Trees
         /// </summary>
         public static TreeNode MergeTrees(TreeNode t1, TreeNode t2)
