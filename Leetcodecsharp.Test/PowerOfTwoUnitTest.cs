a1683ffa0244e33807669f6152fb92a7b04eea9e
trickyrat <wangjian0513@outlook.com>
2021-09-30 17:29:07 +0800

    Add 231. Power of Two testcases

---
+++ Leetcodecsharp.Test/PowerOfTwoUnitTest.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the Trickyrat under one or more agreements.
+// The Trickyrat licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Leetcodecsharp.Test
+{
+    public class PowerOfTwoUnitTest
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(16)]
+        [InlineData(4)]
+        public void Test(int num)
+        {
+            Assert.True(Solution.IsPowerOfTwo(num));
+        }
+
+
+        [Theory]
+        [InlineData(3)]
+        public void Test2(int num)
+        {
+            Assert.False(Solution.IsPowerOfTwo(num));
+        }
+    }
+}
