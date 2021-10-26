ff950ac7a019b0619bd5169bbb45e6166accbd15
trickyrat <wangjian0513@outlook.com>
2021-10-26 17:30:33 +0800

    修改33实现

--- Leetcodecsharp/Solution.cs
+++ Leetcodecsharp/Solution.cs
@@ -1142,19 +1142,45 @@ namespace Leetcodecsharp
         /// <returns></returns>
         public static int Search(int[] nums, int target)
         {
-            int left = 0, right = nums.Length;
-            while (left < right)
+            int n = nums.Length;
+            if (n == 0)
             {
-                int mid = (left + right) / 2;
-                double num = (nums[mid] < nums[0]) == (target < nums[0])
-                                 ? nums[mid]
-                                 : target < nums[0] ? double.NegativeInfinity : double.PositiveInfinity;
-                if (num < target)
-                    left = mid + 1;
-                else if (num > target)
-                    right = mid;
-                else
+                return -1;
+            }
+            if (n == 1)
+            {
+                return nums[0] == target ? 0 : -1;
+            }
+            int l = 0, r = n - 1;
+            while (l <= r)
+            {
+                int mid = (l + r) / 2;
+                if (nums[mid] == target)
+                {
                     return mid;
+                }
+                if (nums[0] <= nums[mid])
+                {
+                    if (nums[0] <= target && target < nums[mid])
+                    {
+                        r = mid - 1;
+                    }
+                    else
+                    {
+                        l = mid + 1;
+                    }
+                }
+                else
+                {
+                    if (nums[mid] < target && target <= nums[n - 1])
+                    {
+                        l = mid + 1;
+                    }
+                    else
+                    {
+                        r = mid - 1;
+                    }
+                }
             }
             return -1;
         }
