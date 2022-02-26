4fbf808e1eda9b8661a74d68ac393bc1609d5114
trickyrat <wangjian0513@outlook.com>
2022-02-27 00:25:17 +0800

    添加一些测试样例

---
+++ Leetcodecsharp.Test/FindMedianSortedArraysUnitTest.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the Trickyrat under one or more agreements.
+// The Trickyrat licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Leetcodecsharp.Test
+{
+    public class FindMedianSortedArraysUnitTest
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 3 }, new int[] { 2 }, 2.00000)]
+        [InlineData(new int[] { 1, 2 }, new int[] { 3, 4 }, 2.50000)]
+        public void MultipleDataTest(int[] A, int[] B, double expected)
+        {
+            double actual = Solution.FindMedianSortedArrays(A, B);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
