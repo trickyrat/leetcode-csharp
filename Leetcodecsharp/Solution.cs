205c05ec73d1546770c4b348663efd799c9be188
trickyrat <wangjian0513@outlook.com>
2022-03-18 22:53:32 +0800

    添加393. UTF-8 Validation

--- Leetcodecsharp/Solution.cs
+++ Leetcodecsharp/Solution.cs
@@ -3499,6 +3499,67 @@ public class Solution
     }
 
     /// <summary>
+    /// 393. UTF-8 Validation
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    public static bool ValidUtf8(int[] data)
+    {
+        int MASK1 = 1 << 7;
+        int MASK2 = (1 << 7) + (1 << 6);
+        int m = data.Length;
+        int index = 0;
+        while (index < m)
+        {
+            int num = data[index];
+            int n = GetBytes(num);
+            if (n < 0 || index + n > m)
+            {
+                return false;
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                if (!IsValid((data[index + i])))
+                {
+                    return false;
+                }
+            }
+
+            index += n;
+        }
+
+        return true;
+
+        int GetBytes(int num)
+        {
+            if ((num & MASK1) == 0)
+            {
+                return 1;
+            }
+
+            int n = 0;
+            int mask = MASK1;
+            while ((num & mask) != 0)
+            {
+                n++;
+                if (n > 4)
+                {
+                    return -1;
+                }
+                mask >>= 1;
+            }
+
+            return n >= 2 ? n : -1;
+        }
+
+        bool IsValid(int num)
+        {
+            return (num & MASK2) == MASK1;
+        }
+    }
+
+    /// <summary>
     /// 401. Binary Watch
     /// </summary>
     public static IList<string> ReadBinaryWatch(int turnedOn)
