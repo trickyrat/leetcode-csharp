a27832321d509529416fb0507138585c55d91826
trickyrat <wangjian0513@outlook.com>
2022-04-11 00:25:43 +0800

    添加357. 统计各位数字都不同的数字个数

---
+++ Leetcode.Test/CountNumbersWithUniqueDigitsUnitTest.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the Trickyrat under one or more agreements.
+// The Trickyrat licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Leetcode.Test
+{
+    public class CountNumbersWithUniqueDigitsUnitTest
+    {
+        [Theory]
+        [InlineData(2, 91)]
+        [InlineData(0, 1)]
+        public void MultipleDataTest(int n, int expected)
+        {
+            int actual = Solution.CountNumbersWithUniqueDigits(n);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
--- Leetcode/Solution.cs
+++ Leetcode/Solution.cs
@@ -4007,6 +4007,30 @@ public class Solution
     }
 
     /// <summary>
+    /// 357. 统计各位数字都不同的数字个数
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public static int CountNumbersWithUniqueDigits(int n) 
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        if(n == 1)
+        {
+            return 10;
+        }
+        int res = 10, cur = 9;
+        for (int i = 0; i < n - 1; i++)
+        {
+            cur *= 9 - i;
+            res += cur;
+        }
+        return res;
+    }
+
+    /// <summary>
     /// 371. Sum of Two Integers
     /// </summary>
     /// <param name="a"></param>
