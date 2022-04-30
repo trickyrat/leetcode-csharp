5391bf60289d6664f0b7b62fd19ede8f80256df9
trickyrat <wangjian0513@outlook.com>
2022-04-30 21:00:38 +0800

    修改启动

---
+++ Leetcode/Startup.cs
@@ -0,0 +1,9 @@
+﻿// Licensed to the Trickyrat under one or more agreements.
+// The Trickyrat licenses this file to you under the MIT license.
+
+namespace Leetcodecsharp;
+
+public class Startup
+{
+    
+}
