cc848569e2f148fe2809d91c484401e0c54efdde
trickyrat <wangjian0513@outlook.com>
2022-05-29 17:09:18 +0800

    add 171. Excel Sheet Column Number

--- Leetcode/Solution.cs
+++ Leetcode/Solution.cs
@@ -3579,6 +3579,24 @@ public class Solution
     }
 
     /// <summary>
+    /// 171. Excel Sheet Column Number
+    /// </summary>
+    /// <param name="columnTitle"></param>
+    /// <returns></returns>
+    public int TitleToNumber(string columnTitle)
+    {
+        int number = 0;
+        int multiple = 1;
+        for (int i = columnTitle.Length - 1; i >= 0; --i)
+        {
+            int k = columnTitle[i] - 'A' + 1;
+            number += k * multiple;
+            multiple *= 26;
+        }
+        return number;
+    }
+
+    /// <summary>
     /// 172. Factorial Trailing Zeroes
     /// </summary>
     /// <param name="n"></param>
