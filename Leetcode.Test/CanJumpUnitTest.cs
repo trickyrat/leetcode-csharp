8b5367705614511f7d6dcff94b06f820333e1ae7
trickyrat <wangjian0513@outlook.com>
2022-08-14 13:37:50 +0800

    ADD testcases

---
+++ Leetcode.Test/CanJumpUnitTest.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the Trickyrat under one or more agreements.
+// The Trickyrat licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Leetcode.Test;
+public class CanJumpUnitTest
+{
+    private readonly Solution _solution;
+
+    public CanJumpUnitTest()
+    {
+        _solution = new Solution();
+    }
+
+    [Theory]
+    [InlineData(new int[] { 2, 3, 1, 1, 4 }, true)]
+    [InlineData(new int[] { 3, 2, 1, 0, 4 }, false)]
+    public void MultipleDataTest(int[] nums, bool expected)
+    {
+        var actual = _solution.CanJump(nums);
+        Assert.Equal(expected, actual);
+    }
+}
+
---
+++ Leetcode.Test/SpiralOrderUnitTest.cs
@@ -0,0 +1,51 @@
+﻿// Licensed to the Trickyrat under one or more agreements.
+// The Trickyrat licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+
+using Xunit;
+
+namespace Leetcode.Test;
+public class SpiralOrderUnitTest
+{
+    private readonly Solution _solution;
+
+    public SpiralOrderUnitTest()
+    {
+        _solution = new Solution();
+    }
+
+    public static IEnumerable<object[]> GetData()
+    {
+        yield return new object[]
+        {
+            new int[][]
+            {
+                new int[] { 1, 2, 3, 4 },
+                new int[] { 5, 6, 7, 8 },
+                new int[] { 9, 10, 11, 12 }
+            },
+            new List<int> { 1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7 }
+        };
+        yield return new object[]
+        {
+             new int[][]
+             {
+                 new int[] { 1, 2, 3 },
+                 new int[] { 4, 5, 6 },
+                 new int[] { 7, 8, 9 }
+             },
+             new List<int> { 1, 2, 3, 6, 9, 8, 7, 4, 5 }
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(GetData))]
+    public void MultipleDataTest(int[][] matrix, IList<int> expected)
+    {
+        var actual = _solution.SpiralOrder(matrix);
+        Assert.Equal(expected, actual);
+    }
+
+}
+
