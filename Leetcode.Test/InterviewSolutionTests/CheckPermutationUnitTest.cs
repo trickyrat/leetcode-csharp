4f056d3662c830839433b814a694d59a5a48007b
trickyrat <wangjian0513@outlook.com>
2022-09-29 01:04:03 +0800

    test: add CheckPermutation testcases

---
+++ Leetcode.Test/InterviewSolutionTests/CheckPermutationUnitTest.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the Trickyrat under one or more agreements.
+// The Trickyrat licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Leetcode.Test.InterviewSolutionTests;
+public class CheckPermutationUnitTest
+{
+    private readonly InterviewSolution _solution;
+
+    public CheckPermutationUnitTest()
+    {
+        _solution = new InterviewSolution();
+    }
+
+    [Theory]
+    [InlineData("abc", "bca", true)]
+    [InlineData("abc", "bad", false)]
+    public void MultipleDataTest(string s1, string s2, bool expected)
+    {
+        var actual = _solution.CheckPermutation(s1, s2);
+        Assert.Equal(expected, actual);
+    }
+}
+
