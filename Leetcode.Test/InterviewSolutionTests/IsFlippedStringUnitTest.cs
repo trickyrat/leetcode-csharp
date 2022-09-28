ad8acb3f28a836c2f89b2d38bbca03c4c49bf500
trickyrat <wangjian0513@outlook.com>
2022-09-29 00:52:47 +0800

    test: add IsFlippedString testcase

---
+++ Leetcode.Test/InterviewSolutionTests/IsFlippedStringUnitTest.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the Trickyrat under one or more agreements.
+// The Trickyrat licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Leetcode.Test.InterviewSolutionTests;
+public class IsFlippedStringUnitTest
+{
+    private readonly InterviewSolution _solution;
+
+    public IsFlippedStringUnitTest()
+    {
+        _solution = new InterviewSolution();
+    }
+
+
+    [Theory]
+    [InlineData("waterbottle", "erbottlewat", true)]
+    [InlineData("aa", "aba", false)]
+    public void MultipleDataTest(string s1, string s2, bool expected)
+    {
+        var actual = _solution.IsFlippedString(s1, s2);
+        Assert.Equal(expected, actual);
+    }
+}
+
