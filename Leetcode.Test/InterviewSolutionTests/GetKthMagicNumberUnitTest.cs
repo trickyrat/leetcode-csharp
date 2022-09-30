44620e5781f097410768edf4d2e9bb37b9b85b18
trickyrat <wangjian0513@outlook.com>
2022-09-30 20:15:58 +0800

    test: add GetKthMagicNumber test cases

---
+++ Leetcode.Test/InterviewSolutionTests/GetKthMagicNumberUnitTest.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the Trickyrat under one or more agreements.
+// The Trickyrat licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Leetcode.Test.InterviewSolutionTests;
+public class GetKthMagicNumberUnitTest
+{
+    private readonly InterviewSolution _solution;
+
+    public GetKthMagicNumberUnitTest()
+    {
+        _solution = new InterviewSolution();
+    }
+
+
+    [Theory]
+    [InlineData(5, 9)]
+    public void MultipleDataTest(int k, int expected)
+    {
+        var actual = _solution.GetKthMagicNumber(k);
+        Assert.Equal(expected, actual);
+    }
+}
+
