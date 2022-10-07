5ccb0a2c6cc67e10ad45039fda7ee833f6e40c81
trickyrat <wangjian0513@outlook.com>
2022-10-07 15:55:18 +0800

    test: add CanTransform test cases

---
+++ Leetcode.Test/SolutionTests/CanTransformUnitTest.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the Trickyrat under one or more agreements.
+// The Trickyrat licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Leetcode.Test.SolutionTests;
+public class CanTransformUnitTest
+{
+    private readonly Solution _solution;
+
+    public CanTransformUnitTest()
+    {
+        _solution = new Solution();
+    }
+
+    [Theory]
+    [InlineData("RXXLRXRXL", "XRLXXRRLX", true)]
+    [InlineData("R", "L", false)]
+    public void MultipleDataTest(string start, string end, bool expected)
+    {
+        var actual = _solution.CanTransform(start, end);
+        Assert.Equal(expected, actual);
+    }
+}
+
