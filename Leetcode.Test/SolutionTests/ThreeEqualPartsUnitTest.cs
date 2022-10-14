81951af856f48ace026d25253ac4a4e4f0db1afc
trickyrat <wangjian0513@outlook.com>
2022-10-14 21:35:19 +0800

    test: add ThreeEqualParts test cases

---
+++ Leetcode.Test/SolutionTests/ThreeEqualPartsUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Leetcode.Test.SolutionTests;
+
+public class ThreeEqualPartsUnitTest
+{
+    private readonly Solution _solution;
+
+    public ThreeEqualPartsUnitTest()
+    {
+        _solution = new Solution();
+    }
+
+    [Theory]
+    [InlineData(new int[] {1, 0, 1, 0, 1}, new int[] {0, 3})]
+    [InlineData(new int[] {1, 1, 0, 1, 1}, new int[] {-1, -1})]
+    [InlineData(new int[] {1, 1, 0, 0, 1}, new int[] {0, 2})]
+    public void Test(int[] arr, int[] expected)
+    {
+        var actual = _solution.ThreeEqualParts(arr);
+        Assert.Equal(expected, actual);
+    }
+}
