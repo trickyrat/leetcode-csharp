5dabf642900a8747cf0e0d0502eefefef1e4d2cc
trickyrat <wangjian0513@outlook.com>
2022-10-14 22:50:37 +0800

    test: add MaxChunksToSorted test cases

---
+++ Leetcode.Test/SolutionTests/MaxChunksToSortedUnitTest.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the Trickyrat under one or more agreements.
+// The Trickyrat licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Leetcode.Test.SolutionTests;
+public class MaxChunksToSortedUnitTest
+{
+    private readonly Solution _solution;
+
+    public MaxChunksToSortedUnitTest()
+    {
+        _solution = new Solution();
+    }
+
+
+    [Theory]
+    [InlineData(new int[] { 4, 3, 2, 1, 0 }, 1)]
+    [InlineData(new int[] { 1, 0, 2, 3, 4 }, 4)]
+    public void MultipleDataTest(int[] arr, int expected)
+    {
+        var actual = _solution.MaxChunksToSorted(arr);
+        Assert.Equal(expected, actual);
+    }
+}
+
