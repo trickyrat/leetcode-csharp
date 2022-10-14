ff8a9ca4cc8b3e43ff6d1085c2ec64e7c8678680
trickyrat <wangjian0513@outlook.com>
2022-10-14 22:10:21 +0800

    feature: add 801. Minimum Swaps To Make Sequences Increasing

--- Leetcode/Solution.cs
+++ Leetcode/Solution.cs
@@ -6537,6 +6537,35 @@ public class Solution
     }
 
     /// <summary>
+    /// 801. Minimum Swaps To Make Sequences Increasing
+    /// </summary>
+    /// <param name="nums1"></param>
+    /// <param name="nums2"></param>
+    /// <returns></returns>
+    public int MinSwap(int[] nums1, int[] nums2)
+    {
+        int n = nums1.Length;
+        int a = 0, b = 1;
+        for (int i = 1; i < n; i++)
+        {
+            int at = a, bt = b;
+            a = b = n;
+            if (nums1[i] > nums1[i - 1] && nums2[i] > nums2[i - 1])
+            {
+                a = Math.Min(a, at);
+                b = Math.Min(b, bt + 1);
+            }
+            if (nums1[i] > nums2[i - 1] && nums2[i] > nums1[i - 1])
+            {
+                a = Math.Min(a, bt);
+                b = Math.Min(b, at + 1);
+            }
+        }
+
+        return Math.Min(a, b);
+    }
+
+    /// <summary>
     /// 804. Unique Morse Code Words
     /// </summary>
     /// <param name="words"></param>
