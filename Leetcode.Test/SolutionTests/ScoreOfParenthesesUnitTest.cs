2b8e74963b5f129526cc69f5abdb237d8387d280
trickyrat <wangjian0513@outlook.com>
2022-10-14 22:06:22 +0800

    test: add ScoreOfParentheses test cases

---
+++ Leetcode.Test/SolutionTests/ScoreOfParenthesesUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Leetcode.Test.SolutionTests;
+
+public class ScoreOfParenthesesUnitTest
+{
+    private readonly Solution _solution;
+
+    public ScoreOfParenthesesUnitTest()
+    {
+        _solution = new Solution();
+    }
+
+    [Theory]
+    [InlineData("()", 1)]
+    [InlineData("(())", 2)]
+    [InlineData("()()", 2)]
+    public void Test(string s, int expected)
+    {
+        var actual = _solution.ScoreOfParentheses(s);
+        Assert.Equal(expected, actual);
+    }
+}
