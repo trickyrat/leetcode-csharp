7904a108faab53eb162d61e0105b50b698f94cdc
trickyrat <wangjian0513@outlook.com>
2022-10-14 22:52:19 +0800

    test: add DistinctSubseqII test cases

---
+++ Leetcode.Test/SolutionTests/DistinctSubseqIIUnitTest.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the Trickyrat under one or more agreements.
+// The Trickyrat licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Leetcode.Test.SolutionTests;
+public class DistinctSubseqIIUnitTest
+{
+    private readonly Solution _solution;
+
+    public DistinctSubseqIIUnitTest()
+    {
+        _solution = new Solution();
+    }
+
+
+    [Theory]
+    [InlineData("abc", 7)]
+    [InlineData("aba", 6)]
+    [InlineData("aaa", 3)]
+    public void MultipleDataTest(string s, int expected)
+    {
+        var actual = _solution.DistinctSubseqII(s);
+        Assert.Equal(expected, actual);
+    }
+}
+
