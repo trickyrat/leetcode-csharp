d01a4dcf5e76146623bd20f1bf939852d9fd995d
trickyrat <wangjian0513@outlook.com>
2022-10-14 22:15:18 +0800

    feature: add 1790. Check if One String Swap Can Make Strings Equal

--- Leetcode/Solution.cs
+++ Leetcode/Solution.cs
@@ -8615,6 +8615,35 @@ public class Solution
     /// <returns></returns>
     public bool CheckOnesSegment(string s) => !s.Contains("01");
 
+    public bool AreAlmostEqual(string s1, string s2)
+    {
+        int n = s1.Length;
+        IList<int> diff = new List<int>();
+        for (int i = 0; i < n; i++)
+        {
+            if (s1[i] != s2[i])
+            {
+                if (diff.Count >= 2)
+                {
+                    return false;
+                }
+                diff.Add(i);
+            }
+        }
+
+        if (diff.Count == 0)
+        {
+            return true;
+        }
+
+        if (diff.Count != 2)
+        {
+            return false;
+        }
+
+        return s1[diff[0]] == s2[diff[1]] && s1[diff[1]] == s2[diff[0]];
+    }
+    
     /// <summary>
     /// 1791. Find Center of Star Graph
     /// </summary>
