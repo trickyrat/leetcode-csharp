5e1ccc0d87771d6c35b2d4cf2e598cb01079f931
trickyrat <wangjian0513@outlook.com>
2022-10-14 17:44:38 +0800

    test: add MinAddToMakeValid test cases

---
+++ Leetcode.Test/SolutionTests/MinAddToMakeValidUnitTest.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the Trickyrat under one or more agreements.
+// The Trickyrat licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Leetcode.Test.SolutionTests;
+public class MinAddToMakeValidUnitTest
+{
+    private readonly Solution _solution;
+
+    public MinAddToMakeValidUnitTest()
+    {
+        _solution = new Solution();
+    }
+
+
+    [Theory]
+    [InlineData("())", 1)]
+    [InlineData("(((", 3)]
+    public void MultipleDataTest(string s, int expected)
+    {
+        var actual = _solution.MinAddToMakeValid(s);
+        Assert.Equal(expected, actual);
+    }
+}
+
