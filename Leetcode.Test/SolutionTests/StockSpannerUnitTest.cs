bdbc6262a90116de538a05ad8e6b657a5af17bc1
trickyrat <wangjian0513@outlook.com>
2022-10-22 22:03:07 +0800

    feature: add 901. Online Stock Span

---
+++ Leetcode.Test/SolutionTests/StockSpannerUnitTest.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the Trickyrat under one or more agreements.
+// The Trickyrat licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Leetcode.Test.SolutionTests;
+public class StockSpannerUnitTest
+{
+    private readonly StockSpanner _stockSpanner;
+
+    public StockSpannerUnitTest()
+    {
+        _stockSpanner = new StockSpanner();
+    }
+
+    [Fact]
+    public void MultipleDataTest()
+    {
+        Assert.Equal(1, _stockSpanner.Next(100));
+        Assert.Equal(1, _stockSpanner.Next(80));
+        Assert.Equal(1, _stockSpanner.Next(60));
+        Assert.Equal(2, _stockSpanner.Next(70));
+        Assert.Equal(1, _stockSpanner.Next(60));
+        Assert.Equal(4, _stockSpanner.Next(75));
+        Assert.Equal(6, _stockSpanner.Next(85));
+    }
+}
+
---
+++ Leetcode/StockSpanner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Leetcode;
+public class StockSpanner
+{
+    public int Index
+    {
+        get; private set;
+    }
+    public Stack<(int day, int price)> Stack
+    {
+        get; set;
+    }
+    public StockSpanner()
+    {
+        Stack = new Stack<(int day, int price)>();
+        Stack.Push((-1, int.MaxValue));
+        Index = -1;
+    }
+
+    public int Next(int price)
+    {
+        Index++;
+        while (price >= Stack.Peek().price)
+        {
+            Stack.Pop();
+        }
+        int res = Index - Stack.Peek().day;
+        Stack.Push((Index, price));
+        return res;
+    }
+}
