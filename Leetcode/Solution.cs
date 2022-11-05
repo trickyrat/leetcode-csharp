8b6267eb50e1e4cee7c2951f511f9103d2973b64
trickyrat <wangjian0513@outlook.com>
2022-11-05 22:30:51 +0800

    feature: add 1235. Maximum Profit in Job Scheduling

--- Leetcode/Solution.cs
+++ Leetcode/Solution.cs
@@ -8026,6 +8026,52 @@ public class Solution
     }
 
     /// <summary>
+    /// 1235. Maximum Profit in Job Scheduling
+    /// </summary>
+    /// <param name="startTime"></param>
+    /// <param name="endTime"></param>
+    /// <param name="profit"></param>
+    /// <returns></returns>
+    public int JobScheduling(int[] startTime, int[] endTime, int[] profit)
+    {
+        int BinarySearch(int[][] jobs, int right, int target)
+        {
+            int left = 0;
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (jobs[mid][1] > target)
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return left;
+        }
+
+        int n = startTime.Length;
+        int[][] jobs = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            jobs[i] = new int[] {startTime[i], endTime[i], profit[i]};
+        }
+
+        Array.Sort(jobs, (a, b) => a[1] - b[1]);
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++)
+        {
+            int k = BinarySearch(jobs, i - 1, jobs[i - 1][0]);
+            dp[i] = Math.Max(dp[i - 1], dp[k] + jobs[i - 1][2]);
+        }
+
+        return dp[n];
+    }
+    
+    /// <summary>
     /// 1249
     /// </summary>
     /// <param name="s"></param>
