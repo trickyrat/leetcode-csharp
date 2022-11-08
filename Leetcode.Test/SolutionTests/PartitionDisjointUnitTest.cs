5851049608c899ebd68e16ef8ad22243afc4ebe9
trickyrat <wangjian0513@outlook.com>
2022-11-08 23:03:39 +0800

    test: add PartitionDisjoint test cases

---
+++ Leetcode.Test/SolutionTests/PartitionDisjointUnitTest.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the Trickyrat under one or more agreements.
+// The Trickyrat licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Leetcode.Test.SolutionTests;
+public class PartitionDisjointUnitTest
+{
+    private readonly Solution _solution;
+
+    public PartitionDisjointUnitTest()
+    {
+        _solution = new Solution();
+    }
+
+
+    [Theory]
+    [InlineData(new int[] { 5, 0, 3, 8, 6 }, 3)]
+    [InlineData(new int[] { 1, 1, 1, 0, 6, 12 }, 4)]
+    public void MultipleDataTest(int[] nums, int expected)
+    {
+        var actual = _solution.PartitionDisjoint(nums);
+        Assert.Equal(expected, actual);
+    }
+}
+
