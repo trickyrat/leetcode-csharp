f5a172d4e48659926e8625579c25775ba532e150
trickyrat <wangjian0513@outlook.com>
2022-12-31 01:28:03 +0800

    feat: add 855. Exam Room

---
+++ Leetcode.Test/SolutionTests/ExamRoomUnitTest.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the Trickyrat under one or more agreements.
+// The Trickyrat licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace LeetCode.Test.SolutionTests;
+public class ExamRoomUnitTest
+{
+
+    [Fact]
+    public void SingleTest()
+    {
+        ExamRoom er = new(10);
+        Assert.Equal(0, er.Seat());
+        Assert.Equal(9, er.Seat());
+        Assert.Equal(4, er.Seat());
+        Assert.Equal(2, er.Seat());
+        er.Leave(4);
+        Assert.Equal(5, er.Seat());
+    }
+
+}
+
---
+++ Leetcode/ExamRoom.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode;
+
+public class ExamRoomComparer : IComparer<(int first, int second)>
+{
+    public int Compare((int first, int second) x, (int first, int second) y)
+    {
+        int d1 = x.second - x.first, d2 = y.second - y.first;
+        return d1 / 2 < d2 / 2 || (d1 / 2 == d2 / 2 && x.first > y.first) ? 1 : -1;
+    }
+}
+
+public class ExamRoom
+{
+    public PriorityQueue<(int first, int second), (int first, int second)> PQ
+    {
+        get; private set;
+    }
+
+    public SortedSet<int> Seats
+    {
+        get; private set;
+    }
+
+    public int N
+    {
+        get; private set;
+    }
+    public ExamRoom(int n)
+    {
+        N = n;
+        Seats = new();
+        PQ = new(new ExamRoomComparer());
+    }
+
+    public int Seat()
+    {
+        if (Seats.Count == 0)
+        {
+            Seats.Add(0);
+            return 0;
+        }
+        int left = Seats.Min, right = N - 1 - Seats.Max;
+        while (Seats.Count >= 2)
+        {
+            var p = PQ.Peek();
+            if (Seats.Contains(p.first) && Seats.Contains(p.second)
+                && Seats.GetViewBetween(p.first + 1, Seats.Max).Min == p.second)
+            {
+                int d = p.second - p.first;
+                if (d / 2 < right || d / 2 <= left)
+                {
+                    break;
+                }
+                PQ.Dequeue();
+                PQ.Enqueue((p.first, p.first + d / 2), (p.first, p.first + d / 2));
+                PQ.Enqueue((p.first + d / 2, p.second), (p.first + d / 2, p.second));
+                Seats.Add(p.first + d / 2);
+                return p.first + d / 2;
+            }
+            PQ.Dequeue();
+        }
+        if (right > left)
+        {
+            PQ.Enqueue((Seats.Max, N - 1), (Seats.Max, N - 1));
+            Seats.Add(N - 1);
+            return N - 1;
+        }
+        else
+        {
+            PQ.Enqueue((0, Seats.Min), (0, Seats.Min));
+            Seats.Add(0);
+            return 0;
+        }
+    }
+
+    public void Leave(int p)
+    {
+        if (p != Seats.Max && p != Seats.Min)
+        {
+            int prev = Seats.GetViewBetween(Seats.Min, p - 1).Max, next = Seats.GetViewBetween(p + 1, Seats.Max).Min;
+            PQ.Enqueue((prev, next), (prev, next));
+        }
+        Seats.Remove(p);
+    }
+}
