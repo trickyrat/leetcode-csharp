f70c27cc3c66b4080cb1105f22baed23beb428e1
trickyrat <wangjian0513@outlook.com>
2022-12-31 15:09:54 +0800

    feat: add 2037. Minimum Number of Moves to Seat Everyone

--- Leetcode/Solution.cs
+++ Leetcode/Solution.cs
@@ -9351,6 +9351,19 @@ public class Solution
     }
 
     /// <summary>
+    /// 2037. Minimum Number of Moves to Seat Everyone
+    /// </summary>
+    /// <param name="seats"></param>
+    /// <param name="students"></param>
+    /// <returns></returns>
+    public int MinMovesToSeat(int[] seats, int[] students)
+    {
+        Array.Sort(seats);
+        Array.Sort(students);
+        return seats.Select((t, i) => Math.Abs(t - students[i])).Sum();
+    }
+
+    /// <summary>
     /// 2044. Count Number of Maximum Bitwise-OR Subsets
     /// </summary>
     /// <param name="nums"></param>
