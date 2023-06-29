59d7dbf0cc4a9ad0e17c1961b56e2e73ba4b75c3
trickyrat <wangjian0513@outlook.com>
2023-06-30 00:41:35 +0800

    Create AddBinaryUnitTest.cs

---
+++ LeetCodecsharp.Test/SolutionTests/AddBinaryUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace LeetCodecsharp.Test.SolutionTests;
+
+public class AddBinaryUnitTest
+{
+    private readonly Solution _solution;
+    public AddBinaryUnitTest()
+    {
+        _solution = new Solution();
+    }
+
+    [Theory]
+    [InlineData("11", "1", "100")]
+    [InlineData("1010", "1011", "10101")]
+    public void MultipleDataTest(string a, string b, string expected)
+    {
+        var actual = _solution.AddBinary(a, b);
+        Assert.Equal(expected, actual);
+    }
+}
