a0b7bdba5000b5451a3cbcbf2fce5b2caa48df1a
trickyrat <wangjian0513@outlook.com>
2024-03-18 20:25:33 +0800

    remove LeetCodecsharp.Infrastructure

--- LeetCodecsharp.Infrastructure/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿// Licensed to the Trickyrat under one or more agreements.
-// The Trickyrat licenses this file to you under the MIT license.
-
-namespace LeetCode.Infrastructure;
-
-public class Class1
-{
-
-}
