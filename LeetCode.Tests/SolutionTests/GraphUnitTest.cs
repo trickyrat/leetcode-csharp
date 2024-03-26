d632295646939998b601567e6ca9a459a97c9e11
trickyrat <wangjian0513@outlook.com>
2024-03-26 22:43:22 +0800

    feat: add 2642. Design Graph With Shortest Path Calculator

---
+++ LeetCode.Tests/SolutionTests/GraphUnitTest.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Tests.SolutionTests;
+
+public class GraphUnitTest
+{
+    [Fact]
+    public void Test()
+    {
+        var graph = new Graph(4, [[0, 2, 5], [0, 1, 2], [1, 2, 1], [3, 0, 3]]);
+        Assert.Equal(6, graph.ShortestPath(3, 2));
+        Assert.Equal(-1, graph.ShortestPath(0, 3));
+        graph.AddEdge([1, 3, 4]);
+        Assert.Equal(6, graph.ShortestPath(0, 3));
+    }
+}
