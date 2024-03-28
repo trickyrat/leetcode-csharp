f8ce5d5024fb28b7a4638de6c32760f2e9f3b289
trickyrat <wangjian0513@outlook.com>
2024-03-28 18:59:56 +0800

    feat: Add 1997. First Day Where You Have Been in All the Rooms

---
+++ LeetCode.Tests/SolutionTests/FirstDayBeenInAllRoomsUnitTest.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Tests.SolutionTests;
+
+public class FirstDayBeenInAllRoomsUnitTest
+{
+    public static TheoryData<int[], int> Data =>
+        new()
+        {
+            { [0, 0], 2 },
+            { [0, 0, 2], 6 },
+            { [0, 1, 2, 0], 6 },
+        };
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void Test(int[] nextVisit, int expected)
+    {
+        var actual = Solution.FirstDayBeenInAllRooms(nextVisit);
+        Assert.Equal(expected, actual);
+    }
+}
