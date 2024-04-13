7dea3e7e236c1489e99018edd1757f4a93e0aaa6
trickyrat <wangjian0513@outlook.com>
2024-04-13 15:29:50 +0800

    feat: Add 2810. Faulty Keyboard

---
+++ LeetCode.Tests/SolutionTests/FinalStringUnitTest.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Tests.SolutionTests;
+
+public class FinalStringUnitTest
+{
+    [Theory]
+    [InlineData("string", "rtsng")]
+    [InlineData("poiinter", "ponter")]
+    public void Test(string input, string expected)
+    {
+        var actual = Solution.FinalString(input);
+        Assert.Equal(expected, actual);
+    }
+}
