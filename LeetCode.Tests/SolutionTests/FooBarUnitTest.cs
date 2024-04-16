3baaf668c25dd195cd33f0c6eafb59681401cf55
trickyrat <wangjian0513@outlook.com>
2024-04-16 15:54:29 +0800

    feat: Add 1115. Print FooBar Alternately

---
+++ LeetCode.Tests/SolutionTests/FooBarUnitTest.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Tests.SolutionTests;
+
+public class FooBarUnitTest
+{
+    public static TheoryData<int, string> Data =>
+        new()
+        {
+            { 1, "foobar" },
+            { 2, "foobarfoobar" },
+            { 4, "foobarfoobarfoobarfoobar" },
+        };
+
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    public async Task Test(int n, string expected)
+    {
+        var foobar = new FooBar(n);
+        string actual = "";
+
+        var fooTask = Task.Run(() => foobar.Foo(() => actual += "foo"));
+        var barTask = Task.Run(() => foobar.Bar(() => actual += "bar"));
+        await Task.WhenAll(fooTask, barTask);
+
+        Assert.Equal(expected, actual);
+    }
+}
