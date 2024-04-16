50641862abc2e04cd3430c761a8f020a866736ba
trickyrat <wangjian0513@outlook.com>
2024-04-16 17:02:04 +0800

    feat: Add 1116. Print Zero Even Odd

---
+++ LeetCode/ZeroEvenOdd.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCode;
+
+public class ZeroEvenOdd
+{
+    private int _n;
+    private readonly SemaphoreSlim _zeroSemaphore = new(1, 1);
+    private readonly SemaphoreSlim _evenSemaphore = new(0, 1);
+    private readonly SemaphoreSlim _oddSemaphore = new(0, 1);
+
+    public ZeroEvenOdd(int n)
+    {
+        _n = n;
+    }
+
+    public void Zero(Action<int> printNumber) 
+    {
+        for (int i = 1; i <= _n; i++) 
+        {
+            _zeroSemaphore.Wait();
+            printNumber(0);
+            if (i % 2 == 0) 
+            {
+                _evenSemaphore.Release();
+            } 
+            else 
+            {
+                _oddSemaphore.Release();
+            }
+        }
+    }
+
+    public void Even(Action<int> printNumber) 
+    {
+        for (int i = 2; i <= _n; i += 2) 
+        {
+            _evenSemaphore.Wait();
+            printNumber(i);
+            _zeroSemaphore.Release();
+        }
+    }
+
+    public void Odd(Action<int> printNumber) 
+    {
+        for (int i = 1; i <= _n; i += 2) 
+        {
+            _oddSemaphore.Wait();
+            printNumber(i);
+            _zeroSemaphore.Release();
+        }
+    }
+}
