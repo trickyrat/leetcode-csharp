c656b19cf934bed19aca814644af2c9e15acc29e
trickyrat <wangjian0513@outlook.com>
2024-04-16 15:54:29 +0800

    feat: Add 1115. Print FooBar Alternately

---
+++ LeetCode/FooBar.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode;
+
+public class FooBar
+{
+    private int _n;
+    private readonly AutoResetEvent _foo = new(true);
+    private readonly AutoResetEvent _bar = new(false);
+
+    public FooBar(int n)
+    {
+        _n = n;
+    }
+
+    public void Foo(Action printFoo)
+    {
+        for (int i = 0; i < _n; i++)
+        {
+            _foo.WaitOne();
+            printFoo();
+            _bar.Set();
+        }
+    }
+
+    public void Bar(Action printBar)
+    {
+        for (int i = 0; i < _n; i++)
+        {
+            _bar.WaitOne();
+            printBar();
+            _foo.Set();
+        }
+    }
+}
