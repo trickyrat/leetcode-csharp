c5ab1280bed7b7a7c3a53a32bc31cea6e23546dd
trickyrat <wangjian0513@outlook.com>
2024-04-27 09:12:11 +0800

    feat: Add 2739. Total Distance Traveled

---
+++ LeetCode.Tests/SolutionTests/DistanceTraveledUnitTest.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Tests;
+
+public class DistanceTraveledUnitTest
+{
+    [Theory]
+    [InlineData(5, 10, 60)]
+    [InlineData(1, 2, 10)]
+    public void Test(int mainTank, int additionalTank, int expected)
+    {
+        var actual = Solution.DistanceTraveled(mainTank, additionalTank);
+        Assert.Equal(expected, actual);
+    }
+}
