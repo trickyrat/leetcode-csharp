7fe3223923c20768a83a9e91a95cfdff36826e87
trickyrat <wangjian0513@outlook.com>
2025-01-12 11:52:38 +0800

    feat: add 2241. Design an ATM Machine

---
+++ LeetCode.Tests/SolutionTests/ATMUnitTest.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace LeetCode.Tests.SolutionTests;
+
+public class ATMUnitTest
+{
+    [Fact]
+    public void Test()
+    {
+        ATM atm = new();
+        atm.Deposit([0, 0, 1, 2, 1]);
+        Assert.Equal([0, 0, 1, 0, 1], atm.Withdraw(600));
+        atm.Deposit([0, 1, 0, 1, 1]);
+        Assert.Equal([-1], atm.Withdraw(600));
+        Assert.Equal([0, 1, 0, 0, 1], atm.Withdraw(550));
+    }
+}
---
+++ LeetCode/ATM.cs
@@ -0,0 +1,47 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace LeetCode;
+
+public class ATM
+{
+    private readonly long[] _count;
+    private readonly long[] _value;
+
+    public ATM()
+    {
+        _count = [0, 0, 0, 0, 0];
+        _value = [20, 50, 100, 200, 500];
+    }
+
+    public void Deposit(int[] banknotesCount)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            _count[i] += banknotesCount[i];
+        }
+    }
+
+    public int[] Withdraw(int amount)
+    {
+        int[] res = new int[5];
+        for (int i = 4; i >= 0; --i)
+        {
+            res[i] = (int)Math.Min(_count[i], amount / _value[i]);
+            amount -= res[i] * (int)_value[i];
+        }
+
+        if (amount > 0)
+        {
+            return [-1];
+        }
+        else
+        {
+            for (int i = 0; i < 5; ++i)
+            {
+                _count[i] -= res[i];
+            }
+            return res;
+        }
+    }
+}
